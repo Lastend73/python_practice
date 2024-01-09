7051ae666213f3ecf5bc5f7800f54be3b4cb7cbe
Lastend73 <kyu23336@gmail.com>
2024-01-09 17:21:39 +0900

    2024-01-09

--- 20240109/e005.py
+++ 20240109/e005.py
@@ -4,3 +4,9 @@ def hello():
 
 def python():
     print("파이썬")
+
+# git 정리
+
+# 최초 실행 -> git init -> git 아이디 이메일 설정  -> git commit -m "내용" -> git branch -m (브랜치 이름 : 기본값 main) -> git remote add origin (깃허브 주소) -> git push -u origin main
+
+# 이후 git add -> git commit -m "내용" -> git push
